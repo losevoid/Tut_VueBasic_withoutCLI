6d8ee8751526f1c0c8d41e28134c45321d2e1a2a
Roni <uranoni777@gmail.com>
2019-05-22 01:31:19 +0800

    add computed in first lecture

--- Ch02_Component/2-3_Props/2-3-1_Pass_Data/component.js
+++ Ch02_Component/2-3_Props/2-3-1_Pass_Data/component.js
@@ -1,15 +1,18 @@
-Vue.component('my-component', {
-    data: function () {
-        return {
-
-        }
-    },
-    // props跟component限制一樣用keybacase
-    props: ['title', 'msg'],
-    template:
-        `<div>
+Vue.component("my-component", {
+  data: function() {
+    return {};
+  },
+  // props跟component限制一樣用keybacase
+  props: ["title", "msg"],
+  computed: {
+    lowcaseMsg() {
+      return this.msg.trim().toLowerCase();
+    }
+  },
+  template: `<div>
     <h3> {{title}} </h3>
     <h6> {{msg}} </h6>
+    <h6>小寫處理 {{lowcaseMsg}} </h6>
     </div>
     `
-})
+});
